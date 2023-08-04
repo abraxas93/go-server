25f3b9905d88de8bc19ba7feb13783f4d39b8a50
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-04 17:37:01 +0545

    removed comments

--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -32,8 +32,6 @@ func (l *Logger) Err(format string, v ...interface{}) {
 }
 
 func CreateLogger(messages map[string]string, flags int) *Logger {
-	// flags := log.LstdFlags | log.Lshortfile
-
 	return &Logger{
 		infoLogger: log.New(os.Stdout, ColorGreen+messages["Info"], flags),
 		warnLogger: log.New(os.Stdout, ColorYellow+messages["Warn"], flags),
