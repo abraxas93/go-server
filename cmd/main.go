633c3b3998d8dbb0921efcaf1bc70dae925e3bc9
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-06 18:36:10 +0545

    removed comments and old code

--- cmd/main.go
+++ cmd/main.go
@@ -1,62 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"go-server/pkg/config"
 	"go-server/pkg/logger"
 )
 
-type Config struct {
-	Port int
-	Env  string
-}
-
 func main() {
 	cfg := config.GetConfig(".env")
-	msgs := make(map[string]string)
-	msgs["Info"] = "INFO: "
-	msgs["Warn"] = "WARN: "
-	msgs["Err"] = "ERROR: "
-	logger.InitLogger(msgs)
+	logger.InitLogger(make(map[string]string))
 	logger, _ := logger.GetLogger()
-	fmt.Printf("%+v\n", cfg)
-	logger.Info("---> %s\n", "my")
-
-	// fmt.Println(convertToFieldName("POSTGRES_DB_USER"))
-	// // Read the .env file
-	// data, err := os.ReadFile(".env")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	// fmt.Println(string(data))
-	// fmt.Println(config.GetConfig())
-	// fmt.Println(strings.Split(string(data), "="))
-	// cfg := Config{Port: 8080, Env: "dev"}
-	// structValue := reflect.ValueOf(&cfg).Elem()
-	// // structType := structValue.Type()
-	// fmt.Println("Struct Type:", structValue)
-	// fieldName := "Port"
-	// fieldValue := reflect.ValueOf(cfg).FieldByName(fieldName)
-	// if fieldValue.IsValid() {
-	// 	fmt.Println(fieldName, "=", fieldValue.Interface())
-	// } else {
-	// 	fmt.Println(fieldName, "not found.")
-	// }
-	// flags := log.LstdFlags | log.Lshortfile
-	// // infoLogger := log.New(os.Stdout, "", flags)
-	// // infoLogger.Printf("This is my string %q\n", "custom string")
-	// msgs := make(map[string]string)
-	// msgs["Info"] = "INFO: "
-	// msgs["Warn"] = "WARN: "
-	// msgs["Err"] = "ERROR: "
-	// logger.InitLogger(msgs, flags)
-	// // fmt.Printf("%T\n", "sdfsg")
-	// logger, err := logger.GetLogger()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
-	// logger.Info("---> %T", "Hello World")
-	// logger.Warn("---> %T", "Hello World")
-	// logger.Err("---> %T", "Hello World")
+	logger.Info("%+v\n", cfg)
 }
