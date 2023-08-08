28164b7f487cb6cd225f65681b9bec9ce8f96de2
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-08 14:38:05 +0545

    connect postgres to project

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,18 @@ package main
 
 import (
 	"go-server/pkg/config"
+	"go-server/pkg/database/postgres"
 	"go-server/pkg/logger"
 )
 
 func main() {
 	cfg := config.GetConfig(".env")
-	logger.InitLogger(make(map[string]string))
 	logger := logger.GetLogger()
 	logger.Info("%+v\n", cfg)
+	_, err := postgres.Connect(cfg.DB.Postgres)
+	if err != nil {
+		logger.Err(err.Error())
+		panic(err)
+	}
 
 }
--- pkg/database/postgres/postgres.go
+++ pkg/database/postgres/postgres.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"go-server/pkg/config"
+	"go-server/pkg/logger"
 
 	_ "github.com/lib/pq"
 )
@@ -21,7 +22,7 @@ func Connect(c config.PostgresConfig) (*sql.DB, error) {
 		db.Close() // Close the connection before returning an error
 		return nil, err
 	}
-
-	fmt.Println("Established a successful connection!")
+	log := logger.GetLogger()
+	log.Info("Established a successful connection!")
 	return db, nil
 }
