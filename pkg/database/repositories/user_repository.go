9b03450da2bea3ae21343e1fc63014be0b48cb80
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-10 13:33:06 +0545

    add ctx usage to UserRepository

--- pkg/database/repositories/user_repository.go
+++ pkg/database/repositories/user_repository.go
@@ -1,6 +1,7 @@
 package repositories
 
 import (
+	"context"
 	"database/sql"
 	"go-server/pkg/database/models"
 )
@@ -13,18 +14,18 @@ func New(db *sql.DB) *UserRepository {
 	return &UserRepository{DB: db}
 }
 
-func (r *UserRepository) CreateUser(name string, password string) error {
-	_, err := r.DB.Exec("INSERT INTO users (name, password) VALUES ($1, $2)", name, password)
+func (r *UserRepository) CreateUser(ctx context.Context, name string, password string) error {
+	_, err := r.DB.ExecContext(ctx, "INSERT INTO users (name, password) VALUES ($1, $2)", name, password)
 	if err != nil {
 		return err
 	}
 	return nil
 }
 
-func (ur *UserRepository) FindByID(id int) (*models.User, error) {
+func (ur *UserRepository) FindByID(ctx context.Context, id int) (*models.User, error) {
 	var user models.User
 	query := "SELECT id, name, password, created_at, updated_at FROM users WHERE id = $1"
-	row := ur.DB.QueryRow(query, id)
+	row := ur.DB.QueryRowContext(ctx, query, id)
 	err := row.Scan(&user.ID, &user.Name, &user.Password, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		return nil, err
