90c1f780660349a34d089e088bc812c3b5b62640
Savchuk Alexandr <savchyk.a@gmail.com>
2023-08-11 14:18:55 +0545

    add encryptPassword fn

--- internal/user/user.go
+++ internal/user/user.go
@@ -1,6 +1,10 @@
 package user
 
-import "time"
+import (
+	"crypto/sha256"
+	"fmt"
+	"time"
+)
 
 type User struct {
 	ID        int
@@ -8,4 +12,11 @@ type User struct {
 	Password  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func (u *User) encryptPassword(password string) {
+	hash := sha256.New()
+	hash.Write([]byte(password))
+	hashed := hash.Sum(nil)
+	u.Password = fmt.Sprintf("%x", hashed)
 }
